5f62fcfbeb9ebc4e6122363ce8e3cc3f80e1044a
alufy26 <alufy04@hotmail.com>
2026-09-05 13:51:09 +0900

    Delete assets/config.js

--- assets/config.js
+++
@@ -1,6 +0,0 @@
-// ここをデプロイしたGASのWebアプリURLと、スクリプトプロパティに設定したAPI_KEYに書き換えてください。
-// 例: GAS_URL: "https://script.google.com/macros/s/AKfycb.../exec"
-const CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbziQ6hjpmPA4Z4zfCaaSm4QA24dxvZi3dXqHkjIpg8hgdYGkp5pMdJeGCFgSm4DURxoAw/exec",
-  API_KEY: "6w3hHudp15.",
-};
